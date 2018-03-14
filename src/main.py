e4350db9725726491f35b0567a4ada6fe9ef24e6
Arthur Revaclier <a.r.revaclier@student.tue.nl>
2018-03-14 13:18:09 +0100

    Changed text to the two possible way of attacking

--- src/main.py
+++ src/main.py
@@ -1,3 +1,3 @@
 from scapy.all import *
-input = input("Choose your preferred attack\n1. Discovery\n2.Stuff\n")
+input = input("Choose your preferred attack\n1. Manual attack\n2. Configure automatic attack\n")
 print(input)
